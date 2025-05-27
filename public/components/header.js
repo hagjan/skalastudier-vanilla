1d5efb05511772ab194b067e6fe4636d259a417d
Jan Erik Hagevold <erik.hagevold@olavstoppen.no>
2025-05-27 08:20:31 +0200

    Add relative base

--- public/components/header.js
+++ public/components/header.js
@@ -13,13 +13,13 @@ class HeaderComponent extends HTMLElement {
               <a href="/">Heim</a>
             </li>
             <li>
-              <a href="/pages/articles.html">Artiklar</a>
+              <a href="pages/articles.html">Artiklar</a>
             </li>
             <li>
-              <a href="/pages/harmonic-navigation.html">Harmonisk Navigasjon</a>
+              <a href="pages/harmonic-navigation.html">Harmonisk Navigasjon</a>
             </li>
             <li>
-              <a href="/pages/resources.html">Ressursar</a>
+              <a href="pages/resources.html">Ressursar</a>
             </li>
           </ul>
         </nav>
